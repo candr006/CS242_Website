1873833fbde0f8b0ac19c322e01e6a7398645823
candr006 <candr006@ucr.edu>
2019-03-07 13:00:38 -0800

    looping through results array

--- html/lucene_index/results.php
+++ html/lucene_index/results.php
@@ -7,8 +7,16 @@
  	//execute shell command that runs indexer using user input
  	//Need to add '2>&1' at the end of your shell command so that the results of the command get output
  	//exec('ls 2>&1',$results);
- 	exec('sh indexer.sh "'.$_POST['search_text'].'" 2>&1',$results);
- 	print_r($results);
+ 	exec('sh indexer.sh "'.$_POST['search_text'].'"',$results);
+ 	//$file = fopen("lucene_output.txt","a");
+ 	//print_r(error_get_last());
+ 	i=0;
+ 	foreach ($results as $line) {
+ 		if(i>8 && i<15)
+	 		fwrite($file,($line."\n"));
+ 		i++;
+ 	}
+ 	fclose($file)
  ?> 
  </body>
 </html>
