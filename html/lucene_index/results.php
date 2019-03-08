f02d73cd4a63358ff1a5680de75aac10e8e294f4
candr006 <candr006@ucr.edu>
2019-03-08 13:46:34 -0800

    more results formatting

--- html/lucene_index/results.php
+++ html/lucene_index/results.php
@@ -17,31 +17,41 @@
 		 <?php
 	 	//execute shell command that runs indexer using user input
 	 	//Need to add '2>&1' at the end of your shell command so that the results of the command get output
-	 	exec('sh indexer.sh "'.$_POST['search_text'].'"',$results);
+		 if(is_array($_POST['search_text'])){
+		 	$search_text=implode(" ",$_POST['search_text']);
+		 }else{
+		 	$search_text=str_replace(',',' ',$_POST["search_text"]);
+		 }
+
+	 	//exec('sh indexer.sh "'.$search_text.'"',$results);
+		$results=file("/var/www/html/lucene_index/lucene_output.txt");
 
 	 	$i=0;
 	 	//change this val if you want to output more top results
 	 	$num_top_results=5;
 
 	 	//start at index 10 because that's where the actual results are
-	 	$last_row=9+$num_top_results;
+	 	$last_row=9+$num_top_results+1;
 	 	foreach ($results as $line) {
-	 		if($i>9 && $i<$last_row){
+	 		//if($i>9 && $i<$last_row){
 
 	 				//split up the output in order the format the results table
 	 				$larr=explode("-->",$line);
 	 				$title_url=explode("--",$larr[1]);
 	 				$title=$title_url[0];
-	 				$url=$title_url[1];
+	 				//$url=$title_url[1];
+	 				$url="https://www.google.com";
+
 		 		?>
 		 		<tr>
-		 			<td><span class="result_name"><?php echo $title ?></span>
+		 			<td><a href=<?php echo '"'.$url.'"'; ?> target="_blank"><span class="result_name"><?php echo $title ?></span>
 		 				<br>
 		 				<span class="result_url"><?php echo $url ?></span>
+		 				</a>
 		 			</td>
 		 		</tr>
 		 		<?php
-	 		}
+	 		//}
 
 	 		$i++;
 	 	}
